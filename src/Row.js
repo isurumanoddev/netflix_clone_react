d763f1546109e0d9bc07db41244bc46be903f338
isurumanod <isurumanod99@gmail.com>
2023-05-21 05:40:49 +0530

    create rows

--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,20 @@
 import React,{useState,useEffect} from 'react';
 
+const base_url ="https://image.tmdb.org/t/p/original/"
+
 
 
 function Row({title,fetchUrl}) {
     
     const [movies,setMovies] = useState([])
+    const [data,setData] = useState(["https://wallpapers.com/images/hd/cool-profile-picture-7aecpj5vptd1a0ib.jpg","https://upload.wikimedia.org/wikipedia/commons/a/a6/Ananya_panday_%28cropped%29.jpg","https://static.toiimg.com/thumb/msid-99302368,width-960,height-1280,resizemode-6.cms"])
 
     useEffect(() => {
         async function fetchData(){
             const request =await fetch(fetchUrl)
             const data = await request.json()
-            console.log(data.results)
+
+            setMovies(data.results)
 
 
             return request
@@ -18,11 +22,23 @@ function Row({title,fetchUrl}) {
         fetchData()
 
 
-    },[])
+    },[fetchUrl])
+
+
+console.log(movies)
+    const movie = movies.map(movie => <img className="row__poster" key={movie.id} src={`${base_url}${movie.poster_path}`}/> )
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+
+
+            <div className="row__posters">
+                {movie}
+
+
+
+            </div>
         </div>
     );
 }
--- src/requests.js
+++ src/requests.js
@@ -1,10 +1,10 @@
 
-const API_KEY = "670d88b5cce04dd40a2a7a9fcc3ea4f";
+const API_KEY = "e670d88b5cce04dd40a2a7a9fcc3ea4f";
 
 const requests = {
-    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchTrending:`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
 
 };
 
